acec51aa95338ebf79828cc899bf618a5613b61d
Arttawer <33116636+Arttawer@users.noreply.github.com>
2020-09-18 00:53:19 +0300

    Initial commit

---
+++ 3lab.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include "Date.h"
+using namespace std;
+
+void InputDate(int& day, int& month, int& year);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int type;
+	int day;
+	int month;
+	int year;
+	int k = 0;
+
+	while (k == 0)
+	{
+		cout << "Выберите тип календаря:\n1. Григорианский\n2. Юлианский\n";  cin >> type;
+		if (type == 1)
+		{
+			while (k == 0) {
+				InputDate(day, month, year);
+				GregorianCalendar gregorianCalendar(day, month, year);
+				gregorianCalendar.GetWeekday();
+				if (gregorianCalendar.IsWeekend())
+					cout << "Выходной\n";
+
+				k++;
+
+				cout << "Введите 0 для новой даты\n"; cin >> k;
+
+			}
+		}
+
+		else
+		{
+			if (type == 2)
+			{
+				while (k == 0) {
+					InputDate(day, month, year);
+					JulianCalendar julianCalendar(day, month, year);
+					julianCalendar.GetWeekday();
+					if (julianCalendar.IsWeekend())
+						cout << "Выходной\n";
+
+					k++;
+					cout << "Введите 0 для новой даты\n"; cin >> k;
+				}
+			}
+
+			else
+				cout << "Выберите корректный тип!\n"; k = 0;
+		}
+	}
+	return 0;
+}
+void InputDate(int& day, int& month, int& year)
+
+{
+	cout << "Введите дату\n";
+	cout << "Год\n";	cin >> year;
+	cout << "Месяц\n";  cin >> month;
+	cout << "День\n";	cin >> day;
+}
---
+++ Date.h
@@ -0,0 +1,48 @@
+#pragma once
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+class Calendar
+{
+protected:
+	int dayResult = 0;
+	int a = 0;
+	int yearcalc = 0;
+	int monthcalc = 0;
+public:
+	virtual void GetWeekday() = 0;
+	virtual int GetDaysCount() = 0;
+	virtual bool IsWeekend() = 0;
+};
+
+class GregorianCalendar :public Calendar
+{
+	int day;
+	int month;
+	int year;
+
+public:
+	GregorianCalendar(int day_, int month_, int year_);
+	void GetWeekday() override;
+	bool IsWeekend() override;
+	int GetDaysCount() override;
+
+};
+
+class JulianCalendar :public Calendar
+{
+	int day;
+	int month;
+	int year;
+
+public:
+
+	JulianCalendar(int day_, int month_, int year_);
+	void GetWeekday() override;
+	bool IsWeekend() override;
+	int GetDaysCount();
+};
+
+
